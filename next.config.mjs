5ddd3c5f0a9ba7c23fd9f58799e875cc73c7afd8
Lucas Aschenbach <Lucas.Aschenbach@gmail.com>
2024-03-30 20:04:40 +0100

    Fix assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   basePath: '/c24-website',
-  assetPrefix: '/c24-website',
+  assetPrefix: '/c24-website/',
 };
 
 export default nextConfig;
