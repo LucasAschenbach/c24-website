33660de71f58bafea805fba1effaaec7c5347024
Lucas Aschenbach <Lucas.Aschenbach@gmail.com>
2024-03-30 21:54:58 +0100

    Cleanup link-tag styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,11 @@
 import type { Metadata } from "next";
-import { Raleway, Inter, Montserrat, Cabin, DM_Sans, DM_Mono, Merriweather } from "next/font/google";
+import { Raleway, Inter, Montserrat } from "next/font/google";
 import "./globals.css";
 
 // const inter = Inter({ subsets: ["latin"] });
 const raleway = Raleway({ subsets: ["latin"] });
 const inter = Inter({ subsets: ["latin"] });
 const montserrat = Montserrat({ subsets: ["latin"] });
-const cabin = Cabin({ subsets: ["latin"] });
-const dmSans = DM_Sans({ subsets: ["latin"] });
-const dmMono = DM_Mono({
-  subsets: ["latin"],
-  weight: "300"
-});
 
 export const metadata: Metadata = {
   title: "TUM Blockchain Conference 24",
